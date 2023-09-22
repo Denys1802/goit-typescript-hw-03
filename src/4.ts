8db22a04ff907cfc206a6f26503c72bbe5961b85
DenysZyrianov <78175281+Denys1802@users.noreply.github.com>
2023-09-22 12:29:42 +0300

    add files 3 and 4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,52 @@
+class Key{
+	constructor(private signature: number =Math.random()) {}
+
+	getSignature(): number {
+		return this.signature
+	}
+}
+
+class Person {
+	constructor(private key: Key) {}
+
+	getKey(): Key {
+		return this.key
+	}
+}
+
+abstract class House{
+	protected door: boolean = false;
+	private tenants: Person[] = [];
+	protected key: Key | null = null
+
+	constructor(key: Key) {
+        this.key = key;
+	}
+	
+
+	comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log(`Entered the house.`);
+        } else {
+            console.log("The door is closed. Cannot enter.");
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key?.getSignature()) {
+            this.door = true;
+            console.log("The door is open.");
+        } else {
+            console.log("Invalid key. Cannot open the door.");
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
