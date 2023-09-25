0e83f46eca801fe777a1db691b11652c0f09376b
DenysZyrianov <78175281+Denys1802@users.noreply.github.com>
2023-09-25 09:54:07 +0300

    fix 2 and 3 tasks

--- src/2.ts
+++ src/2.ts
@@ -20,15 +20,15 @@ class Employee {
   constructor(public name: string, private department: string, protected salary: number) {
   }
 
-  getEmployeeDetails() {
+  getEmployeeDetails(): string {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
   }
 }
 
 class Manager extends Employee {
 	// Реалізуйте конструктор та збільшіть salary на 10000
-	constructor(salary: number) {
-		super('',"", salary + 10000)
+	constructor(name: string, department: string, salary: number) {
+		super(name,department, salary + 10000)
 	}
 }
 
